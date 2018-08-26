81fa6d4448db73e9b256bf2df9ecbb993c86cba6
j-som <308401057@qq.com>
2018-08-26 23:47:43 +0800

    init

---
+++ php/data/verify_data.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * 一些验证的硬编码常量
+ */
+class VerifyData
+{
+	public static $TOKEN = "happy679";
+}
+?>
---
+++ php/wx_check.php
@@ -0,0 +1,26 @@
+<?php
+
+$signature = $_GET["signature"];
+$timestamp = $_GET["timestamp"];
+$nonce = $_GET["nonce"];
+$echostr = $_GET["echostr"];
+
+if (checkSignature($signature, $timestamp, $nonce)) {
+	echo $echostr;
+}else{
+	echo "";
+};
+
+private function checkSignature($signature, $timestamp, $nonce)
+{
+	$tmpArr = array(VerifyData::$TOKEN, $timestamp, $nonce);
+	sort($tmpArr, SORT_STRING);
+	$tmpStr = implode( $tmpArr );
+	$tmpStr = sha1( $tmpStr );
+	if ($signature == $tmpStr) {
+		return true;
+	} else{
+		return false;
+	}
+}
+?>
