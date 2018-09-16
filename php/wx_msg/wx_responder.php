3e84c833bac1ebd83ec6b981e59a9277e8486a04
j-som <308401057@qq.com>
2018-09-16 19:09:36 +0800

    test

--- php/wx_msg/wx_responder.php
+++ php/wx_msg/wx_responder.php
@@ -73,14 +73,14 @@ class TextResponder extends WxResponder
 				'userId' => "xskj"
 			)
 		));
-		$post_data = http_build_query($json);
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $apr_url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFLELDS, $post_data);
-		$data = curl_exec($ch);
-		return $data;
+		// $post_data = http_build_query($json);
+		// $ch = curl_init();
+		// curl_setopt($ch, CURLOPT_URL, $apr_url);
+		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		// curl_setopt($ch, CURLOPT_POST, 1);
+		// curl_setopt($ch, CURLOPT_POSTFLELDS, $post_data);
+		// $data = curl_exec($ch);
+		return $json;
 	}
 }
 ?>
