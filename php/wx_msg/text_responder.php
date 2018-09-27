851fca48cc08a6189cef945127906e1690b13605
j-som <308401057@qq.com>
2018-09-27 10:24:48 +0800

    test

--- php/wx_msg/text_responder.php
+++ php/wx_msg/text_responder.php
@@ -13,9 +13,10 @@ class TextResponder extends WxResponder
 	public function respond()
 	{
 		$content = $this->msg->getProperty("Content");
-		// $user_id = $this->msg->getFromUserName();
-		// $user_id = str_replace("_","", $user_id);
-		$tuling123 = new Tuling123("userid");
+		$user_id = $this->msg->getFromUserName();
+		$user_id = str_replace("_","", $user_id);
+		
+		$tuling123 = new Tuling123($user_id);
 		$tuling123->talk_to_tuling_ai($content);
 		$reply_type = $tuling123->get_reply_type();
 		switch ($reply_type) {
@@ -28,7 +29,10 @@ class TextResponder extends WxResponder
 				break;
 			
 			default:
-				# code...
+				$now = time();
+				$respond = $tuling123->get_reply_content();
+				$format = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content></xml>";
+				$respond_str = sprintf($format, $user_id, $this->msg->getToUserName(), $now, $reply_type);
 				break;
 		}
 	}
